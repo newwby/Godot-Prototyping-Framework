4842593fe4aca8a61a5a0524f5d3c152f145d101
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2024-07-10 12:02:29 +0100

    GlobalBase

---
+++ src/gpf/classes/class_global.gd
@@ -0,0 +1,33 @@
+extends Node
+
+class_name GlobalBase
+
+##############################################################################
+#
+# GameGlobal is the base class of all DDAT Globals
+# Its purpose is twofold.
+#	1) Avoid duplication of code between globals.
+#	2) Allow configuration options to be easily set on all globals.
+
+##############################################################################
+
+# variants
+
+##############################################################################
+
+# virt
+
+func _ready():
+	process_mode = PROCESS_MODE_ALWAYS
+
+
+###############################################################################
+
+# public
+
+###############################################################################
+
+# private
+
+###############################################################################
+
