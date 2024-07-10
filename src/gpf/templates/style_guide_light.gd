b98fe3ca52d17332081de260c4d3f736d1c5de6a
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2024-07-10 12:15:06 +0100

    Update style_guide_light.gd

--- src/gpf/templates/style_guide_light.gd
+++ src/gpf/templates/style_guide_light.gd
@@ -4,15 +4,26 @@ extends Node
 
 ##############################################################################
 
-##############################################################################
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# docs/todo
 
 ##############################################################################
 
-# virt
+#05. signals
+#06. enums
+#07. constants
+#
+#08. exported variables
+#09. public variables
+#10. private variables
+#11. onready variables
+
+##############################################################################
+
+# setters/getters
+
+##############################################################################
+
+# virts
 
 
 # Called when the node enters the scene tree for the first time.
