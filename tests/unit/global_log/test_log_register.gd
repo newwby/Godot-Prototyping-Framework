be9a75c145093401f40e55775653f551f129490e
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2024-07-24 10:26:23 +0100

    Update test_log_register.gd

--- tests/unit/global_log/test_log_register.gd
+++ tests/unit/global_log/test_log_register.gd
@@ -4,26 +4,31 @@ extends GutTest
 # This test sends an info log message then checks the log register to confirm
 #	whether the log message was accurately stored
 
+##############################################################################
+
 #//TODO update with parameterised tests
+
+##############################################################################
 
 var log_register_tickstamp := 0
 var test_log_string := "testing log registration"
 #//TODO update for non-info log testing or add tests for other log testing
 var test_log_code := "INFO"
 
+##############################################################################
+
+
+# send test log and disable log output behaviour to minimise console spam
 func before_all():
+	GlobalLog.allow_log_output = false
 	log_register_tickstamp = Time.get_ticks_msec()
 	GlobalLog.info(self, test_log_string)
-	if GlobalLog.log_register.has(self):
-		var log_register_entry = GlobalLog.log_register[self]
-		if typeof(log_register_entry) == TYPE_ARRAY:
-			print()
-			print(log_register_entry)
-			print()
-			print("time for log register entry should be close to '{0}'".format([Time.get_ticks_msec()]))
-			print("code for log register entry should be '{0}'".format(["INFO"]))
-			print("output for log register entry should be '{0}'".format([test_log_string]))
-			print("most recent log register entry = {0}".format([log_register_entry[-1]]))
+
+# re-enable log output behaviour afterwards
+func after_all():
+	GlobalLog.allow_log_output = true
+
+##############################################################################
 
 
 func test_log_register_entry_key_exists() -> void:
@@ -59,3 +64,4 @@ func _get_log_register_entry(arg_key: String):
 					return most_recent_entry[arg_key]
 	# else
 	return null
+
