7fbfdea7c93ee5914be51a8093e305d6e621fb65
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2024-07-24 10:25:57 +0100

    GutTest script templates

---
+++ addons/gpf_core/templates/guttest/test_structure.gd
@@ -0,0 +1,65 @@
+# meta-name: GutTest Structured
+# meta-description: Provides a placeholder structure for GutTest scripts
+# meta-default: true
+# meta-space-indent: 4
+
+extends _BASE_
+
+# add broad test description here
+
+##############################################################################
+
+# add any #//TODO here
+
+##############################################################################
+
+# test-scoped variants here
+
+##############################################################################
+
+# test meta methods
+
+
+# alias for prerun_setup
+#func before_all():
+#	pass
+
+
+# alias for setup
+#func before_each():
+#	pass
+
+
+# alias for postrun_teardown
+#func after_all():
+#	pass
+
+
+# alias for teardown
+#func after_each():
+#	pass
+
+
+##############################################################################
+
+# tests
+
+
+# Called when the node enters the scene tree for the first time.
+#func _ready():
+#	pass # Replace with function body.
+
+
+# Called every frame. 'arg_delta' is the elapsed time since the previous frame.
+#func _process(arg_delta):
+#	pass
+
+
+##############################################################################
+
+# test methods should always start with test_
+
+
+#func test_() -> void:
+#    pass
+
