a66b8a270de7efec6e2a86b63ff0deed7f2a8820
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2024-07-24 10:18:44 +0100

    log count tests updated to differentiate call/output

--- tests/unit/global_log/test_log_count.gd
+++ tests/unit/global_log/test_log_count.gd
@@ -1,13 +1,60 @@
 #class_name class
 extends GutTest
 
+# checks if the log call and log count increments correctly on various log types
+# specifically excludes GlobalLog.critical calls as those will crash the
+#	debugger - test that functionality manually
 
-func test_log_count() -> void:
-	# separate test in console
+# test if log call is incrementing even without output/registration
+func test_log_calls() -> void:
+	# no need to record or log spam when testing calls
+	GlobalLog.allow_log_output = false
+	GlobalLog.allow_log_registration = false
 	var test_log_string := "testing log counting"
-	GlobalLog.info(self, test_log_string)
-	var initial_log_count := GlobalLog.total_log_calls
+	
+	var pre_test_log_count := GlobalLog.total_log_calls
+	var expected_post_test_log_count = pre_test_log_count + 1
 	GlobalLog.info(self, test_log_string)
 	var updated_log_count := GlobalLog.total_log_calls
-	assert_eq(updated_log_count, initial_log_count + 1)
+	assert_eq(updated_log_count, expected_post_test_log_count)
+	
+	pre_test_log_count = GlobalLog.total_log_calls
+	expected_post_test_log_count = pre_test_log_count + 1
+	GlobalLog.warning(self, test_log_string)
+	updated_log_count = GlobalLog.total_log_calls
+	assert_eq(updated_log_count, expected_post_test_log_count)
+	
+	pre_test_log_count = GlobalLog.total_log_calls
+	expected_post_test_log_count = pre_test_log_count + 1
+	GlobalLog.error(self, test_log_string)
+	updated_log_count = GlobalLog.total_log_calls
+	assert_eq(updated_log_count, expected_post_test_log_count)
+	
+	# re-enable flags
+	GlobalLog.allow_log_output = true
+	GlobalLog.allow_log_registration = true
+
+
+# test if log counter is incrementing on successful log call
+func test_log_counts() -> void:
+	# cannot disable log output for testing log output count, log spam required
+	var test_log_string := "test log please ignore"
+	
+	var pre_test_log_count := GlobalLog.total_log_output
+	var expected_post_test_log_count = pre_test_log_count + 1
+	GlobalLog.info(self, test_log_string)
+	var updated_log_count := GlobalLog.total_log_output
+	assert_eq(updated_log_count, expected_post_test_log_count)
+	
+	pre_test_log_count = GlobalLog.total_log_output
+	expected_post_test_log_count = pre_test_log_count + 1
+	GlobalLog.warning(self, test_log_string)
+	updated_log_count = GlobalLog.total_log_output
+	assert_eq(updated_log_count, expected_post_test_log_count)
+	
+	pre_test_log_count = GlobalLog.total_log_output
+	expected_post_test_log_count = pre_test_log_count + 1
+	GlobalLog.error(self, test_log_string)
+	updated_log_count = GlobalLog.total_log_output
+	assert_eq(updated_log_count, expected_post_test_log_count)
 
