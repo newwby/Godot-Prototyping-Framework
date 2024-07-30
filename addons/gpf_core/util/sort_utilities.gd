52bf4087351892f6362b16a2ea75ac23d6808a3d
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2024-07-30 10:38:32 +0100

    ArrayUtility -> SortUtility

--- addons/gpf_core/util/sort_utilities.gd
+++ addons/gpf_core/util/sort_utilities.gd
@@ -1,24 +1,24 @@
 extends Node
 
-class_name ArrayUtility
+class_name SortUtility
 
 ##############################################################################
 
-# ArrayUtility is collection of static array management methods
+# SortUtility is collection of static array sorting methods
 
 ##############################################################################
 
 # public
 
 
-static func sort_ascending(a, b):
-	if a[0] < b[0]:
+static func sort_ascending(arg_a, arg_b):
+	if arg_a[0] < arg_b[0]:
 		return true
 	return false
 
 
-static func sort_descending(a, b):
-	if a[0] > b[0]:
+static func sort_descending(arg_a, arg_b):
+	if arg_a[0] > arg_b[0]:
 		return true
 	return false
 
