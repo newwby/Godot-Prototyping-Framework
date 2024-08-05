892bba481fb1858052409d11d4cf574ebac68a0e
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2024-08-05 16:47:10 +0100

    autodoc test

--- addons/gpf_core/util/sort_utilities.gd
+++ addons/gpf_core/util/sort_utilities.gd
@@ -12,12 +12,38 @@ class_name SortUtility
 
 
 static func sort_ascending(arg_a, arg_b):
+	"""sort_ascending returns a list of :class:`bluepy.blte.Service` objects representing
+	the services offered by the device. This will perform Bluetooth service
+	discovery if this has not already been done; otherwise it will return a
+	cached list of services immediately..
+
+	:param uuids: A list of string service UUIDs to be discovered,
+		defaults to None
+	:type uuids: list, optional
+	:return: A list of the discovered :class:`bluepy.blte.Service` objects,
+		which match the provided ``uuids``
+	:rtype: list On Python 3.x, this returns a dictionary view object,
+		not a list
+	"""
 	if arg_a[0] < arg_b[0]:
 		return true
 	return false
 
 
 static func sort_descending(arg_a, arg_b):
+	"""sort_descending returns a list of :class:`bluepy.blte.Service` objects representing
+	the services offered by the device. This will perform Bluetooth service
+	discovery if this has not already been done; otherwise it will return a
+	cached list of services immediately..
+
+	:param uuids: A list of string service UUIDs to be discovered,
+		defaults to None
+	:type uuids: list, optional
+	:return: A list of the discovered :class:`bluepy.blte.Service` objects,
+		which match the provided ``uuids``
+	:rtype: list On Python 3.x, this returns a dictionary view object,
+		not a list
+	"""
 	if arg_a[0] > arg_b[0]:
 		return true
 	return false
