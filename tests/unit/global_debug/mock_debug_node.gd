80ac658a7a2c6a628fedcd2451b1961e34c016f5
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2024-08-15 16:47:14 +0100

    get debug action test

---
+++ tests/unit/global_debug/mock_debug_node.gd
@@ -0,0 +1,22 @@
+extends Node
+
+class_name MockDebugNode
+
+## <class_doc>
+## test data for running global_debug tests on 
+
+##############################################################################
+
+# test properties for DebugValues
+
+var placeholder_value_1 = 42.0
+var placeholder_value_2 = "hello world!"
+
+##############################################################################
+
+# test methods for DebugActions
+
+## placeholder for adding DebugAction
+func placeholder_debug_method() -> void:
+	print("alert - the placeholder debug method was called!")
+
--- tests/unit/global_debug/test_debug_elements.gd
+++ tests/unit/global_debug/test_debug_elements.gd
@@ -11,6 +11,33 @@ extends GutTest
 
 # test-scoped variants here
 
+var test_object = MockDebugNode.new()
+
+##############################################################################
+
+# test meta methods
+
+
+# alias for prerun_setup
+func before_all():
+	get_tree().root.add_child(test_object)
+
+
+# alias for setup
+#func before_each():
+#	pass
+
+
+# alias for postrun_teardown
+#func after_all():
+#	pass
+
+
+# alias for teardown
+#func after_each():
+#	pass
+
+
 ##############################################################################
 
 # tests
@@ -18,17 +45,25 @@ extends GutTest
 
 ## just tests the debug_actions record is being updated
 func test_debug_action_registered() -> void:
+	var test_method = test_object.placeholder_debug_method
 	assert_eq(GlobalDebug.debug_actions.size(), 0)
-	GlobalDebug.add_debug_action(self, "_placeholder_debug_element_action", "test_debug_action")
+	# this fails setup test due to this script not actually being in the scene tree
+	# try adding a fake test class (stub) with the method
+	
+	# separated for better information
+	#assert_eq(NodeUtility.is_valid_in_tree(test_object), true)
+	assert_eq(is_instance_valid(test_object), true)
+	assert_eq(test_object.is_inside_tree(), true)
+	
+	GlobalDebug.add_debug_action(test_object, test_method, "key_test_debug_action")
 	assert_eq(GlobalDebug.debug_actions.size(), 1)
-
-
-##############################################################################
-
-# test-dependent private method
-
-
-## placeholder for adding DebugAction during test_debug_action_registered 
-func _placeholder_debug_element_action() -> void:
-	pass
-
+	var fetch_action = GlobalDebug.get_debug_action("key_test_debug_action")
+	if fetch_action == null:
+		fail_test("could not fetch DebugAction")
+		return
+	
+	assert_eq(fetch_action is DebugAction, true)
+	assert_eq(fetch_action.owner == test_object, true)
+	assert_eq(fetch_action.method == test_method, true)
+	if fetch_action is DebugAction:
+		fetch_action.get_action()
