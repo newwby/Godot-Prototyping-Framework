c9e2959d11ba7a88c687bd44453482abace4b456
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2024-08-17 22:30:03 +0100

    update dbg action is_valid check to consider method

--- addons/gpf_core/debug/debug_action.gd
+++ addons/gpf_core/debug/debug_action.gd
@@ -21,7 +21,7 @@ var method := Callable()
 
 func _init(arg_owner: Node, arg_method: Callable, arg_key, arg_name: String = "", arg_category: String = ""):
 	super(arg_owner, arg_key, arg_name, arg_category)
-	if is_valid:
+	if NodeUtility.is_valid_in_tree(arg_owner):
 		if arg_method.is_valid():
 			if arg_method.get_object() == arg_owner:
 				self.method = arg_method
@@ -48,4 +48,12 @@ func get_action() -> void:
 				format([owner, key, name, method]))
 	else:
 		_is_invalid()
+
+
+## DebugActions require a valid reference to a node inside the tree,
+## as well as a valid method callable reference.
+func is_valid() -> bool:
+	var is_in_tree = super()
+	var is_method_set = method.is_valid()
+	return is_in_tree and is_method_set
 
