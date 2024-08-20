ebcddcf6f9c0568acf86d6551ec22b9cce96a3c5
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2024-08-20 20:37:50 +0100

    debug value tests

--- tests/unit/global_debug/test_debug_elements.gd
+++ tests/unit/global_debug/test_debug_elements.gd
@@ -30,8 +30,9 @@ func before_all():
 
 
 # alias for setup
-#func before_each():
-#	pass
+func before_each():
+	_validate_test_debug_nodes()
+	GlobalDebug.clear_all_debug_elements()
 
 
 # alias for postrun_teardown
@@ -52,7 +53,6 @@ func before_all():
 ## tests the debug_actions record is being updated
 ## tests it can be retrieved
 func test_fetch_debug_action() -> void:
-	_validate_test_debug_action_nodes()
 	_add_test_debug_actions()
 	
 	var fetch_action = GlobalDebug.get_debug_action("key_test_debug_action")
@@ -67,12 +67,40 @@ func test_fetch_debug_action() -> void:
 		fetch_action.get_action()
 
 
+## tests the debug_actions record is being updated
+## tests it can be retrieved
+func test_fetch_debug_value() -> void:
+	_add_test_debug_values()
+	
+	#//TODO move scope of these expected values and keys
+	### IS NOT ASSIGNING TO DEBUG_VALUES
+	var fetch_action_1 = GlobalDebug.get_debug_value("key_test_object_property_1")
+	var fetch_action_2 = GlobalDebug.get_debug_value("key_test_object_property_2")
+	if fetch_action_1 == null:
+		fail_test("could not fetch DebugValue for key: key_test_object_property_1")
+		return
+	if fetch_action_2 == null:
+		fail_test("could not fetch DebugValue for key: key_test_object_property_2")
+		return
+	
+	assert_eq(fetch_action_1 is DebugValue, true)
+	assert_eq(fetch_action_2 is DebugValue, true)
+	assert_eq(fetch_action_1.owner == test_object, true)
+	assert_eq(fetch_action_2.owner == test_object, true)
+	# check against expected values
+	#//TODO move scope of these expected values and keys
+	if fetch_action_1 is DebugValue:
+		assert_eq(fetch_action_1.get_value(), 42.0)
+	if fetch_action_2 is DebugValue:
+		assert_eq(fetch_action_2.get_value(), "hello world!")
+
+
 ##############################################################################
 
 # test called private methods
 
 
-## method to setup test structure
+## method to setup test structure for DebugAction tests
 func _add_test_debug_actions() -> void:
 	var test_method_1 = test_object.placeholder_debug_method
 	var test_method_2 = test_object_false_1.placeholder_debug_method
@@ -90,9 +118,30 @@ func _add_test_debug_actions() -> void:
 	assert_eq(GlobalDebug.debug_actions.size(), 4)
 
 
+## method to setup test structure for DebugValue tests
+func _add_test_debug_values() -> void:
+	var test_property_1_name = "placeholder_value_1"
+	var test_property_1_expected_value = 42.0
+	var test_property_2_name = "placeholder_value_2"
+	var test_property_2_expected_value = "hello world!"
+	assert_eq(GlobalDebug.debug_values.size(), 0)
+	## adds multiple DebugAction objects to make sure we're returning the right one (test_object under key_test_debug_action)
+	## assert test after each for successful add
+	#(arg_owner: Node, arg_property: String, arg_key, arg_name: String = "", arg_category: String = ""):
+	GlobalDebug.add_debug_value(test_object, test_property_1_name, "key_test_object_property_1")
+	assert_eq(GlobalDebug.debug_values.size(), 1)
+	GlobalDebug.add_debug_value(test_object, test_property_2_name, "key_test_object_property_2")
+	assert_eq(GlobalDebug.debug_values.size(), 2)
+
+
 ## method to set up test structure
-func _validate_test_debug_action_nodes() -> void:
+func _validate_test_debug_nodes() -> void:
 	# separated for better information
 	#assert_eq(NodeUtility.is_valid_in_tree(test_object), true)
 	assert_eq(is_instance_valid(test_object), true)
 	assert_eq(test_object.is_inside_tree(), true)
+
+
+## just tests the debug_actions record is being updated
+#func test_debug_action_registered() -> void:
+
