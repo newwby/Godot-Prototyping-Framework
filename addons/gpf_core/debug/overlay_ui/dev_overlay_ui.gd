aaee48697c120b0961ef50c8db373559afae26e3
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2024-08-23 17:31:39 +0100

    bugfix command & overlay ui mixed up

--- addons/gpf_core/debug/overlay_ui/dev_overlay_ui.gd
+++ addons/gpf_core/debug/overlay_ui/dev_overlay_ui.gd
@@ -1,5 +1,16 @@
 extends MarginContainer
 
+class_name DevDebugOverlay
+
+##############################################################################
+
+## <class_doc>
+## Inspired by debugging overlay of Minecraft (© Mojang)
+
+#//TODO update position based on overlay_position property and position changed signal
+
+##############################################################################
+
 const OVERLAY_LABEL_FSTRING := "{0}: {1}" # property: value
 
 # DebugValue : Label
@@ -8,11 +19,18 @@ var active_debug_value_nodes = {}
 @onready var root_overlay_label = $Margin/TopLeftRoot/OverlayRootLabel
 @onready var label_holder_top_left = $Margin/TopLeftRoot
 
+##############################################################################
+
+# virtuals
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
 	GlobalDebug.value_added.connect(_populate_overlay)
 
+
+#===========================================
+# For testing only
+#//TODO remove
 var dv_test = 1
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _input(event):
@@ -22,8 +40,17 @@ func _input(event):
 			GlobalDebug.add_debug_value(self, "dv_test", "dv_test{0}".format([dv_test]))
 			#_populate_overlay()
 			dv_test += 1
+#===========================================
 
 
+##############################################################################
+
+# private
+
+
+## creates label nodes for overlay
+## each label node is parented underneath a specific container that controls
+## its position on the overlay, based on the overlay_position property
 func _populate_overlay(arg_new_dv) -> void:
 	if arg_new_dv is DebugValue:
 		if not arg_new_dv in active_debug_value_nodes:
@@ -35,6 +62,8 @@ func _populate_overlay(arg_new_dv) -> void:
 				new_dv_label.text = OVERLAY_LABEL_FSTRING.format([arg_new_dv.property, arg_new_dv.get_value()])
 
 
+## updates values on overlay when overlay is first shown or every frame whilst
+## overlay is shown
 func _update_overlay() -> void:
 	pass
 	#new_dv_label.text = dv.get_value()
