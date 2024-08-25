c22ddd181f6b95c29106518fcecee50013ace21c
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2024-08-25 17:50:57 +0100

    positional arguments for overlay labels implemented

--- addons/gpf_core/debug/overlay_ui/dev_overlay_ui.gd
+++ addons/gpf_core/debug/overlay_ui/dev_overlay_ui.gd
@@ -8,22 +8,30 @@ class_name DevDebugOverlay
 ## Inspired by debugging overlay of Minecraft (© Mojang)
 
 #//TODO update position based on overlay_position property and position changed signal
+#//TODO update active_debug_value_nodes when DebugValues are removed
+#//TODO add config/pluginb setting for show/hide overlay key
 
 ##############################################################################
 
-## hardcoded action key 
+## hardcoded action key to show/hide the 
 const SHOW_HIDE_OVERLAY_KEY := KEY_F1
-
+## display string for overlay labels
 const OVERLAY_LABEL_FSTRING := "{0}: {1}" # property: value
+## path of the overlay label root node scene
+const OVERLAY_LABEL_SCENE_PATH := "res://addons/gpf_core/debug/overlay_ui/overlay_root_label.tscn"
 
-# DebugValue : Label
+## root label to instance from for overlay labels
+var root_overlay_label := load(OVERLAY_LABEL_SCENE_PATH)
+
+## Register of overlay labels, stored by DebugValues.
+## Is updated whenever GlobalDebug adds a new DebugValue
+## Data structure is {DebugValue : Label, ...}
 var active_debug_value_nodes = {}
 
-@onready var root_overlay_label = $OverlayRootLabel
-@onready var label_holder_top_left = $VBox/Margin/HBox/VBoxLeft/TL/LabelRoot
-@onready var label_holder_top_right = $VBox/Margin/HBox/VBoxRight/TR/LabelRoot
-@onready var label_holder_bottom_left = $VBox/Margin/HBox/VBoxLeft/BL/LabelRoot
-@onready var label_holder_bottom_right = $VBox/Margin/HBox/VBoxRight/BR/LabelRoot
+@onready var label_holder_top_left = $VBox/Margin/HBox/ScrollLeft/VBox/TL/LabelRoot
+@onready var label_holder_top_right = $VBox/Margin/HBox/ScrollRight/VBox/TR/LabelRoot
+@onready var label_holder_bottom_left = $VBox/Margin/HBox/ScrollLeft/VBox/BL/LabelRoot
+@onready var label_holder_bottom_right = $VBox/Margin/HBox/ScrollRight/VBox/BR/LabelRoot
 
 ##############################################################################
 
@@ -46,10 +54,25 @@ func _input(event):
 				visible = !visible
 				# TESTINGG HANDLING
 			elif event.keycode == KEY_F4:
-				print("yes ", dv_test)
-				GlobalDebug.add_debug_value(self, "dv_test", "dv_test{0}".format([dv_test]))
-				#_populate_overlay()
+				#print()
+				#for key in active_debug_value_nodes.keys():
+					#print("{0}: {1}".format([key, active_debug_value_nodes[key]]))
+				#print()
 				dv_test += 1
+				print("yes ", dv_test)
+				var pos = DebugValue.POSITION.BOTTOM_LEFT
+				if dv_test %  4 == 0:
+					pos = DebugValue.POSITION.BOTTOM_LEFT
+				elif dv_test %  3 == 0:
+					pos = DebugValue.POSITION.TOP_RIGHT
+				elif dv_test %  2 == 0:
+					pos = DebugValue.POSITION.TOP_LEFT
+				else:
+					pos = DebugValue.POSITION.BOTTOM_RIGHT
+				GlobalDebug.add_debug_value(self, "dv_test", "dv_test{0}".format([dv_test]),
+						"", "", pos)
+				#_populate_overlay()
+
 #===========================================
 
 
@@ -68,17 +91,14 @@ func _populate_overlay(arg_new_debug_value) -> void:
 				GlobalLog.error(self, "_populate_overlay error; root_overlay_label nullref")
 			else:
 				# duplicate the root (root node defaults to hidden so show the new)
-				var new_dv_label = root_overlay_label.duplicate()
-				label_holder_top_left.call_deferred("add_child", new_dv_label)
+				var new_dv_label = root_overlay_label.instantiate()
+				new_dv_label.name = arg_new_debug_value.property
 				new_dv_label.visible = true
 				# update register
 				active_debug_value_nodes[arg_new_debug_value] = new_dv_label
 				# update the new label node
+				_update_position(arg_new_debug_value, true)
 				_update_label(arg_new_debug_value)
-				## testing logic
-				await new_dv_label.tree_entered
-				arg_new_debug_value.overlay_position = DebugValue.POSITION.BOTTOM_RIGHT
-				_update_position(arg_new_debug_value)
 
 
 func _update_label(arg_debug_value: DebugValue):
@@ -94,27 +114,52 @@ func _update_label(arg_debug_value: DebugValue):
 			debug_label.text = OVERLAY_LABEL_FSTRING.format([debug_property_str, debug_value_str])
 
 
-func _update_position(arg_debug_value: DebugValue):
+func _update_position(arg_debug_value: DebugValue, arg_first_call: bool = false):
 	if not arg_debug_value in active_debug_value_nodes.keys() or (arg_debug_value == null):
 		GlobalLog.error(self, "DebugOverlay _update_position invalid DebugValue passed")
 		return
 	else:
 		var debug_label = active_debug_value_nodes[arg_debug_value]
 		if debug_label is Label:
+			# overlay position to use
 			var debug_label_position = arg_debug_value.overlay_position
+			# default arguments
 			var new_root = null
+			var text_orient_h = HORIZONTAL_ALIGNMENT_LEFT
+			var text_orient_v = VERTICAL_ALIGNMENT_TOP
+			
+			# set text alignment and label root node based on overlay position
 			match debug_label_position:
+				# TL
 				DebugValue.POSITION.TOP_LEFT:
 					new_root = label_holder_top_left
+					text_orient_h = HORIZONTAL_ALIGNMENT_LEFT
+					text_orient_v = VERTICAL_ALIGNMENT_TOP
+				# BL
 				DebugValue.POSITION.BOTTOM_LEFT:
 					new_root = label_holder_bottom_left
+					text_orient_h = HORIZONTAL_ALIGNMENT_LEFT
+					text_orient_v = VERTICAL_ALIGNMENT_BOTTOM
+				# TR
 				DebugValue.POSITION.TOP_RIGHT:
 					new_root = label_holder_top_right
+					text_orient_h = HORIZONTAL_ALIGNMENT_RIGHT
+					text_orient_v = VERTICAL_ALIGNMENT_TOP
+				# BR
 				DebugValue.POSITION.BOTTOM_RIGHT:
 					new_root = label_holder_bottom_right
+					text_orient_h = HORIZONTAL_ALIGNMENT_RIGHT
+					text_orient_v = VERTICAL_ALIGNMENT_BOTTOM
+			
+			# check is valid
 			if is_instance_valid(new_root) and is_instance_valid(debug_label):
 				#TODO get and verify current parent
-				NodeUtility.reparent_node(debug_label.get_parent(), new_root)
+				debug_label.horizontal_alignment = text_orient_h
+				debug_label.vertical_alignment = text_orient_v
+				if arg_first_call:
+					new_root.call_deferred("add_child", debug_label)
+				else:
+					NodeUtility.reparent_node(debug_label.get_parent(), new_root)
 
 
 ## updates values on overlay when overlay is first shown or every frame whilst
