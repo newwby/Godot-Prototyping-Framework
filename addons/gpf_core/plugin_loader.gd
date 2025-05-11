eae00af921bf17e2432346c78b054914cc3528fc
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2025-05-11 21:22:11 +0100

    add plugin project settings

--- addons/gpf_core/plugin_loader.gd
+++ addons/gpf_core/plugin_loader.gd
@@ -17,6 +17,13 @@ const AUTOLOAD_PATHS := {
 	#"GlobalFunc": "res://addons/gpf/autoload/global_functions.gd",
 }
 
+const SETTING_PATH_FORMAT := "addons/prototype_framework/{0}"
+
+# setting name: default value
+const SETTINGS := {
+	"data path": "data"
+}
+
 ##############################################################################
 
 # setters/getters
@@ -29,11 +36,16 @@ const AUTOLOAD_PATHS := {
 func _enter_tree():
 	for name_key in AUTOLOAD_PATHS:
 		add_autoload_singleton(name_key, AUTOLOAD_PATHS[name_key])
+	for setting_key in SETTINGS.keys():
+		var default_value = SETTINGS[setting_key]
+		ProjectSettings.set_setting(SETTING_PATH_FORMAT.format([setting_key]), default_value)
 
 
 func _exit_tree():
 	for name_key in AUTOLOAD_PATHS:
 		remove_autoload_singleton(name_key)
+	for setting_key in SETTINGS.keys():
+		ProjectSettings.set_setting(SETTING_PATH_FORMAT.format([setting_key]), null)
 
 
 ##############################################################################
