b37a7ee37da03267108b1f69b53481cd2203536e
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2025-07-10 22:10:12 +0100

    add verification for local data directories

--- addons/gpf_core/plugin_loader.gd
+++ addons/gpf_core/plugin_loader.gd
@@ -23,11 +23,15 @@ extends EditorPlugin
 
 
 func _enter_tree():
+	# initialise singletons & project settings
 	for name_key in GPFPlugin.AUTOLOAD_PATHS:
 		add_autoload_singleton(name_key, GPFPlugin.AUTOLOAD_PATHS[name_key])
 	for setting_key in GPFPlugin.SETTINGS.keys():
 		var default_value = GPFPlugin.SETTINGS[setting_key]
 		ProjectSettings.set_setting(GPFPlugin.SETTING_PATH_FORMAT.format([setting_key]), default_value)
+	
+	# create local directories for the plugin
+	_verify_local_data_directory()
 
 
 func _exit_tree():
@@ -49,3 +53,16 @@ static func get_data_path_setting() -> String:
 ##############################################################################
 
 # private
+
+
+# if running in editor, and local directories don't exist, create them
+func _verify_local_data_directory() -> void:
+	var data_directory := ProjectSettings.get_setting(GPFPlugin.get_data_path_setting())
+	var local_path = "res://{0}".format([data_directory])
+	var local_dir = DirAccess.open(local_path)
+	if not local_dir:
+		DirAccess.make_dir_recursive_absolute(local_path)
+	var schema_path := "{0}/{1}".format([local_path, "_schema"])
+	var schema_dir = DirAccess.open(schema_path)
+	if not schema_dir:
+		DirAccess.make_dir_recursive_absolute(schema_path)
