46b82dd16ec05cb5eb7578ab75ce8ed7a9bc7d28
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2025-07-13 14:40:11 +0100

    remove test button from plugin data panel

--- addons/gpf_core/scenes/plugin_data_panel.gd
+++ addons/gpf_core/scenes/plugin_data_panel.gd
@@ -1,6 +1,39 @@
 @tool
-extends Button
+extends Control
+
+##############################################################################
+
+#04. # dependencies, docstring
+
+##############################################################################
+
+# signals, enums, constants
+# exported, public, private, onready
+
+@onready var tree: Tree = $Tree
+
+##############################################################################
+
+# setters/getters
+
+##############################################################################
+
+# constructor & virtuals
+
+func _ready() -> void:
+	visibility_changed.connect(_on_visibility_changed)
+
+##############################################################################
+
+# public methods
+
+##############################################################################
+
+# private methods
 
 
-func _on_pressed():
-	print("Hello from the main screen plugin!")
+func _on_visibility_changed() -> void:
+	if is_visible_in_tree():
+		print("hello I'm visible")
+	else:
+		print("data panel hidden")
