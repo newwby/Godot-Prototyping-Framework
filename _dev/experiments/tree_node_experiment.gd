6dcd2ced5915d338aa42f1253adbc800fd950465
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2025-07-13 20:55:23 +0100

    experiment - display spreadsheet from globalData

--- _dev/experiments/tree_node_experiment.gd
+++ _dev/experiments/tree_node_experiment.gd
@@ -1,16 +1,96 @@
 extends Control
 
+#####################################################################
+
+const DB_FIELD_MIN_WIDTH := 50
+
+var tree_root: TreeItem
+
+@onready var tree = $Tree
+
+#####################################################################
+
 func _ready() -> void:
-	pass
-	#var all_data := Data.data_collection
-	#for i in all_data:
-		#print(i)
-	
-	var tree = Tree.new()
-	var root = tree.create_item()
+	_setup_tree()
+	_populate_database()
+
+
+func _setup_tree() -> void:
+	tree_root = tree.create_item()
 	tree.hide_root = true
-	var child1 = tree.create_item(root)
-	var child2 = tree.create_item(root)
-	var subchild1 = tree.create_item(child1)
-	subchild1.set_text(0, "Subchild1")
-	self.call_deferred("add_child", tree)
+	#TreeViewColumn 
+	var columns := Data.EXPECTED_DATA_STRUCTURE.keys()
+	tree.columns = columns.size()
+	for i in range(columns.size()):
+		tree.set_column_title(i, columns[i])
+		tree.set_column_expand(i, false)
+		tree.set_column_custom_minimum_width(i, DB_FIELD_MIN_WIDTH)
+		tree.set_column_expand(i, true)
+	
+	# show tree headers
+	var header_row = tree.create_item(tree_root)
+	for i in range(columns.size()):
+		header_row.set_text(i, columns[i])
+		header_row.set_editable(i, false)
+	
+	tree.item_edited.connect(_on_tree_item_edited)
+	
+
+func _populate_database() -> void:
+	for item in Data.data_collection:
+		if typeof(item) == TYPE_DICTIONARY:
+			_populate_record(item)
+
+
+# record is validated if it's from global Data registers
+# adds record to the database view
+func _populate_record(arg_data: Dictionary) -> void:
+	var columns := Data.EXPECTED_DATA_STRUCTURE.keys()
+	var row = tree.create_item(tree_root)
+	for i in range(columns.size()):
+		var key = columns[i]
+		var value = arg_data.get(key, "")
+		row.set_text(i, str(value))
+		row.set_editable(i, true)
+		row.set_autowrap_mode(i, TextServer.AUTOWRAP_WORD_SMART)
+
+
+func _on_tree_item_edited(item: TreeItem, column: int, new_text: String) -> void:
+	print("{0} - {1} - {2}".format([item, column, new_text]))
+
+
+#func _old_ready() -> void:
+	#var root = tree.create_item()
+	## ► 4. One TreeItem per record.
+	#for rec in records:
+		#var row = tree.create_item(root)
+		#for col_i in cols.size():
+			#var key = cols[col_i]
+			#var value = null
+			#match key:
+				#"data.name":
+					#value = rec.data.get("name", "")
+				#"data.cost":
+					#value = rec.data.get("cost", "")
+				#"data.damage":
+					#value = rec.data.get("damage", "")
+				#"data.range":
+					#value = rec.data.get("range", "")
+				#_ :
+					#value = rec.get(key, "")
+			#row.set_text(col_i, str(value))
+			#row.set_editable(col_i, true)         # make cell writable if you want
+#
+	## ► 5. (Optional) update the underlying dictionary when the user edits a cell.
+	#tree.item_edited.connect(_on_tree_item_edited)
+#
+#func _on_tree_item_edited(item: TreeItem, column: int, new_text: String) -> void:
+	#var row_index = item.get_index()            # index inside root
+	#var key       = cols[column]
+	#var rec       = records[row_index]
+	#match key:
+		#"data.name":   rec.data["name"]   = new_text
+		#"data.cost":   rec.data["cost"]   = float(new_text)
+		#"data.damage": rec.data["damage"] = float(new_text)
+		#"data.range":  rec.data["range"]  = float(new_text)
+		#_: rec[key] = new_text
