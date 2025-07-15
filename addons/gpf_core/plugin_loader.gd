017d2ef3911ad7ec9cca0cd19c915ede8c85f966
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2025-07-15 21:53:09 +0100

    bugfix plugin load order
    tidies methods as well

--- addons/gpf_core/plugin_loader.gd
+++ addons/gpf_core/plugin_loader.gd
@@ -27,39 +27,21 @@ var plugin_panel_instance
 # virts
 
 
+func _disable_plugin():
+	_remove_plugin_panel()
+
+
+# order of operations very important
 func _enter_tree():
-	# initialise singletons & project settings
-	for name_key in GPFPlugin.AUTOLOAD_PATHS:
-		add_autoload_singleton(name_key, GPFPlugin.AUTOLOAD_PATHS[name_key])
-	for setting_key in GPFPlugin.SETTINGS.keys():
-		var default_value = GPFPlugin.SETTINGS[setting_key]
-		ProjectSettings.set_setting(GPFPlugin.SETTING_PATH_FORMAT.format([setting_key]), default_value)
-	
-	# create local directories for the plugin
+	_add_settings()
 	_verify_local_data_directory()
-	# add the plugin database editor tab
+	_add_autoloads()
 	_add_plugin_panel()
 
 
 func _exit_tree():
-	# remove singletons and project settings
-	for name_key in GPFPlugin.AUTOLOAD_PATHS:
-		remove_autoload_singleton(name_key)
-	for setting_key in GPFPlugin.SETTINGS.keys():
-		ProjectSettings.set_setting(GPFPlugin.SETTING_PATH_FORMAT.format([setting_key]), null)
-	
-	# remove the plugin database editor tab
-	_remove_plugin_panel()
-
-
-func _add_plugin_panel():
-	plugin_panel_instance = DATABASE_SCENE.instantiate()
-	# Add the main panel to the editor's main viewport and hide it initially.
-	EditorInterface.get_editor_main_screen().add_child(plugin_panel_instance)
-	_make_visible(false)
-
-
-func _disable_plugin():
+	_remove_settings()
+	_remove_autoloads()
 	_remove_plugin_panel()
 
 
@@ -82,11 +64,6 @@ func _make_visible(visible):
 		plugin_panel_instance.visible = visible
 
 
-func _remove_plugin_panel():
-	if plugin_panel_instance:
-		plugin_panel_instance.queue_free()
-
-
 ##############################################################################
 
 # public
@@ -99,6 +76,40 @@ static func get_data_path_setting() -> String:
 ##############################################################################
 
 # private
+
+
+func _add_autoloads() -> void:
+	for name_key in GPFPlugin.AUTOLOAD_PATHS:
+		add_autoload_singleton(name_key, GPFPlugin.AUTOLOAD_PATHS[name_key])
+
+
+func _add_plugin_panel():
+	plugin_panel_instance = DATABASE_SCENE.instantiate()
+	# Add the main panel to the editor's main viewport and hide it initially.
+	EditorInterface.get_editor_main_screen().add_child(plugin_panel_instance)
+	_make_visible(false)
+
+
+func _add_settings() -> void:
+	for setting_key in GPFPlugin.SETTINGS.keys():
+		var default_value = GPFPlugin.SETTINGS[setting_key]
+		ProjectSettings.set_setting(GPFPlugin.SETTING_PATH_FORMAT.format([setting_key]), default_value)
+
+
+func _remove_autoloads() -> void:
+	# remove singletons and project settings
+	for name_key in GPFPlugin.AUTOLOAD_PATHS:
+		remove_autoload_singleton(name_key)
+
+
+func _remove_plugin_panel():
+	if plugin_panel_instance:
+		plugin_panel_instance.queue_free()
+
+
+func _remove_settings() -> void:
+	for setting_key in GPFPlugin.SETTINGS.keys():
+		ProjectSettings.set_setting(GPFPlugin.SETTING_PATH_FORMAT.format([setting_key]), null)
 
 
 # if running in editor, and local directories don't exist, create them
