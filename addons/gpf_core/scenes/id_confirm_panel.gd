0c1520ca9c55a674817a23c605fe34b77f277010
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2025-08-10 12:24:23 +0100

    open panel method for id confirm panel

--- addons/gpf_core/scenes/id_confirm_panel.gd
+++ addons/gpf_core/scenes/id_confirm_panel.gd
@@ -1,1 +1,15 @@
 extends PopupPanel
+
+@onready var author_edit = %AuthorEdit
+@onready var package_edit = %PackageEdit
+@onready var name_edit = %NameEdit
+
+
+func open_panel(author_text: String, package_text: String, name_text: String):
+	if is_instance_valid(author_edit):
+		author_edit.text = author_text
+	if is_instance_valid(package_edit):
+		package_edit.text = package_text
+	if is_instance_valid(name_edit):
+		name_edit.text = name_text
+	popup_centered()
