d9277f789730a98d96cfb6f3ddd869f349ef79ad
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2025-08-10 22:36:27 +0100

    add signal & button for id_confirm_panel script

--- addons/gpf_core/scenes/id_confirm_panel.gd
+++ addons/gpf_core/scenes/id_confirm_panel.gd
@@ -1,8 +1,13 @@
 extends PopupPanel
 
+signal id_changed(author, package, name)
+
 @onready var author_edit = %AuthorEdit
 @onready var package_edit = %PackageEdit
 @onready var name_edit = %NameEdit
+
+@onready var confirm_button = %Confirm
+@onready var cancel_button = %Cancel
 
 
 func open_panel(author_text: String, package_text: String, name_text: String):
@@ -13,3 +18,23 @@ func open_panel(author_text: String, package_text: String, name_text: String):
 	if is_instance_valid(name_edit):
 		name_edit.text = name_text
 	popup_centered()
+
+
+func _on_confirm_button_pressed():
+	var author_text = ""
+	var package_text = ""
+	var name_text = ""
+	
+	if is_instance_valid(author_edit):
+		author_text = author_edit.text
+	if is_instance_valid(package_edit):
+		package_text = package_edit.text
+	if is_instance_valid(name_edit):
+		name_text = name_edit.text
+	
+	hide()
+	id_changed.emit(author_text, package_text, name_text)
+
+
+func _on_cancel_button_pressed():
+	hide()
