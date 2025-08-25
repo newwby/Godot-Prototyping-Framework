8c61e9bf373b30def6808164fd26582830c46e34
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2025-08-25 15:11:48 +0100

    tidies schema editor script

--- addons/gpf_core/scenes/plugin_database/schema_root.gd
+++ addons/gpf_core/scenes/plugin_database/schema_root.gd
@@ -4,6 +4,8 @@ extends MarginContainer
 
 # docs
 
+#//TODO need to track schema ID
+
 ##############################################################################
 
 # variables
@@ -12,16 +14,8 @@ const DB_FIELD_MIN_WIDTH := 100.0
 
 var tree_root: TreeItem
 
-# data entry paths are indexed by the tree item displaying their value
-# {TreeItem: String}
-#//TODO need to add path indexing to globalData for schemaFiles
-var uid_map := {}
-
 # keys() includes versions
 var active_version_list := {}
-
-# column headers
-var tree_columns := []
 
 @onready var database_tree = %SchemaDBTree
 @onready var schema_editing_panel = %SchemaEditingPanel
@@ -42,11 +36,13 @@ func _ready():
 # public methods
 
 
+# calls the editing panel
 func open_editing_panel(version_id, version_data):
 	if is_instance_valid(schema_editing_panel):
 		schema_editing_panel.open_panel(version_id, version_data)
 
 
+# changes the displayed schema (list of versions & their schema values)
 func select_schema_id(schema_id_name):
 	active_version_list = Data.schema_register.get(schema_id_name)
 	if typeof(active_version_list) == TYPE_DICTIONARY:
@@ -61,9 +57,8 @@ func select_schema_id(schema_id_name):
 # private methods
 
 
-# delete all tree items
+# delete all tree items/rows
 func _clear_tree() -> void:
-	uid_map.clear()
 	var child := tree_root.get_first_child()
 	while child:
 		var next = child.get_next()
@@ -72,12 +67,12 @@ func _clear_tree() -> void:
 
 
 func _initial_tree_setup() -> void:
+	# tree setup
 	database_tree.clear()
 	database_tree.hide_root = true
-	#database_tree.item_edited.connect(_on_tree_item_edited)
-	#database_tree.item_selected.connect(_on_tree_item_selected)
 	tree_root = database_tree.create_item()
 	
+	# setup the schema id filter
 	if is_instance_valid(filter_schema_id):
 		for schema_id in Data.schema_register.keys():
 			filter_schema_id.add_item(schema_id)
@@ -93,35 +88,32 @@ func _load_version_item(version_key) -> void:
 	if typeof(version_data) != TYPE_DICTIONARY:
 		Log.error(self, "type error in _load_version_item")
 		return
-	#var new_tree_item := TreeItem
-	#tree_root.add_child(new_tree_item)
+	
+	# create new
 	var new_row: TreeItem = database_tree.create_item(tree_root)
 	if new_row == null:
 		return
 	
-	# schema version key
+	# set schema version key
 	new_row.set_text(0, str(version_key))
-	# schema data values
+	# set schema data values, array decoded as comma separated string
 	var schema_data_values = Data.decode_tags(version_data.keys())
 	new_row.set_text(1, schema_data_values)
 	
-	# both
+	# both aren't editable
 	for idx in range(2):
 		new_row.set_autowrap_mode(idx, TextServer.AUTOWRAP_WORD_SMART)
 		new_row.set_tooltip_text(idx, "")
 		new_row.set_editable(idx, false)
-	
-	#//TODO update path/uid_map
 
 
 func _load_tree_structure() -> void:
 	# refresh the tree root
 	_clear_tree()
-	# tree needs to list all versions
-	
-	tree_columns = ["version", "values"]
+	# column headers are always the same for schema
+	var tree_columns = ["version", "values"]
 	database_tree.columns = tree_columns.size()
-	
+	# min value for version, rest of the space for values
 	for i in range(tree_columns.size()):
 		database_tree.set_column_title(i, tree_columns[i])
 		database_tree.set_column_custom_minimum_width(i, DB_FIELD_MIN_WIDTH)
@@ -131,10 +123,14 @@ func _load_tree_structure() -> void:
 
 
 func _on_add_version_pressed():
-	open_editing_panel(
-		"1.0",
-		{"First Key": "", "Second Key": 1.0, "Third Key": []}
-		)
+	# pass faked data to the schema editing panel
+	var placeholder_version := ""
+	var placeholder_data := {
+		"String": "",
+		"Float": 1.0,
+		"Array": []
+	}
+	open_editing_panel(placeholder_version, placeholder_data)
 
 
 func _on_create_new_pressed():
@@ -169,23 +165,17 @@ func _on_filter_schema_id_item_selected(index):
 # need to force overwrite the entry for that version in active_schema
 #	then push that change back to the file
 func _on_schema_editing_panel_update_schema(reference_version_id, updated_version_data):
-	#print("bkup")
+	#//TODO temp comparison
 	var original_data = active_version_list.get(reference_version_id, {})
-	#var modified_data = updated_version_data.get(reference_version_id, {})
 	for key in updated_version_data:
 		if original_data.has(key):
 			print("key is ", key, " and new value is ", updated_version_data[key], " and old value is ", original_data[key])
-	
-	#var new_version_schema = active_version_list
-	#new_version_schema.set(reference_version_id, updated_version_data)
-	#print("old = {0}\nnew = {1}".format([active_version_list, new_version_schema]))
-	
+
 
 func _reload_database() -> void:
 	_load_tree_structure()
 	_load_data_list()
 	call_deferred("_select_first_item")
-	#call_deferred("_on_tree_item_selected")
 
 
 # called to default the selection to top of spreadsheet when sheet is reloaded
